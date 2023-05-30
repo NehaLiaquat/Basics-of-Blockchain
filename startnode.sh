1495354533132c6c3ff830032a060d224c633aaf
Neha Liaquat <daniyalqureshi268@gmail.com>
2023-05-30 09:53:05 +0800

    Basics of blockchain

---
+++ startnode.sh
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --miner.threads=1 --datadir "." --nodiscover --http --http.port "8545" --port "30303" --http.corsdomain "*" --nat "any" --http.api eth,web3,personal,net --allow-insecure-unlock --unlock 0 --password ./password.sec --ipcpath "~/.ethereum/geth.ipc"
